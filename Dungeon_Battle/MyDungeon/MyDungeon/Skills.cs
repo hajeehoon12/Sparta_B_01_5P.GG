4280269477d8c59a06ae9517c2c1f9437d98646c
doosansh <doosan5357@gmail.com>
2024-04-30 17:04:31 +0900

    Create Skills.cs

---
+++ Dungeon_Battle/MyDungeon/MyDungeon/Skills.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDungeon
+{
+    internal class Skills
+    {
+        public string Name { get; }
+        public int Attack { get; } 
+        public int Cooldown { get; } 
+        public int CurrentCooldown { get; set; } 
+        public Action<Player> Effect { get; } 
+    }
+}
